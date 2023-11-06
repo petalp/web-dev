409de3a21bd2c48b208da1ddc8ce934d4199e02f
petalp <peteralpha09@gmail.com>
2023-11-06 23:26:30 +0000

    My first Website
    Basic Science design

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let departments = document.getElementById('department');
+
+departments.addEventListener("click", ()=>{
+    let deparment = document.getElementById('departments');
+    deparment.classList.toggle('active');
+})
